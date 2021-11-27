32bed8de2cbc1799439c7985d3c1d007716e9a29
noorhaqs <47667171+noorhaqs@users.noreply.github.com>
2021-11-27 02:52:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests, os, bs4
+
+url = 'https://xkcd.com' # starting url
+os.makedirs('xkcd', exist_ok=True) # store comics in ./xkcd
+
+while not url.endswith('#'):
+  print(f'Downloading page {url}...')
+  res = requests.get(url)
+  res.raise_for_status()
+  soup = bs4.BeautifulSoup(res.text, 'html.parser')
+  comicElem = soup.select('#comic img')
+  if comicElem == []:
+	  print('Could not find comic image.')
+  else:
+	  comicUrl = 'https:' + comicElem[0].get('src')
+	  print(f'Downloading image {comicUrl}...')
+  res = requests.get(comicUrl)
+  res.raise_for_status()
+  imageFile = open(os.path.join('xkcd', 	os.path.basename(comicUrl)),'wb')
+  for chunk in res.iter_content(100000):
+	  imageFile.write(chunk)
+  imageFile.close()
+	  # Get the Prev button's url.
+  prevLink = soup.select('a[rel="prev"]')[0]
+  url = 'https://xkcd.com' + prevLink.get('href')
+
+
+
+print('Done.')
